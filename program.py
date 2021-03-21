cfe34d769feca434516ca1a96dc87079cd0aba56
Alvin Mantovani <alvin.ictn@gmail.com>
2021-03-21 10:14:59 +0700

    add sample of geeksgod @alvin-ictn

--- program.py
+++ program.py
@@ -20,6 +20,6 @@ allsoupcourse = soup.findAll('h3',{'class':'entry-title td-module-title'})
 for item in allsoupcourse:
     linktovisit = item.find('a')['href']
     print(linktovisit)
-    content = parserURL(linktovisit)
+    #content = parserURL(linktovisit)
     #print(content.find('a',{'class':'btn btn-purplex btn-effect mt15 mb5'})['href'])
     #(content)
